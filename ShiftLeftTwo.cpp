48ca763994a8fef855056f11f83d1bfe5f9da14c
Jack Ditto <jack@Jacks-MacBook-Pro.local>
2020-11-11 18:28:00 -0500

    Add code to ShiftLeftTwo

---
+++ ShiftLeftTwo.cpp
@@ -0,0 +1,15 @@
+#include "ShiftLeftTwo.h"
+
+// Constructor
+ShiftLeftTwo::ShiftLeftTwo()
+{
+}
+
+string ShiftLeftTwo::get(string input)
+{
+
+    // Do the shift here
+    string result = "";
+
+    return result;
+}
