10e56387bd6fe9f97ef609252c9462bbd4ea3149
Jack Ditto <jack@Jacks-MacBook-Pro.local>
2020-11-15 22:05:12 -0500

    Finish ShiftLeftTwo code

--- ShiftLeftTwo.cpp
+++ ShiftLeftTwo.cpp
@@ -1,20 +1,57 @@
 #include "ShiftLeftTwo.h"
 
-// Constructor
+/**
+ * Default constructor
+ */
 ShiftLeftTwo::ShiftLeftTwo()
 {
+    this->unitNum = 1;
 }
 
+/**
+ * Constructor that specifies which ShiftLeftTwo unit this is. 
+ * Basically, 
+ *  unitNum 0 = conversion from 26 bits to 28 bits
+ *  unitNum 1 = 32 bits to 32 bits
+ */
+ShiftLeftTwo::ShiftLeftTwo(int unitNum)
+{
+    this->unitNum = unitNum;
+}
+
+/**
+ * Execute and return the shift left two function
+ */
 string ShiftLeftTwo::get()
 {
     this->shiftLeftTwo();
     return this->output;
 }
 
-void ShiftLeftTwo::set(string input) {
+/**
+ * Set the binary string to be shifted left two
+ */
+void ShiftLeftTwo::set(string input)
+{
     this->input = input;
 }
 
-void ShiftLeftTwo::shiftLeftTwo() {
-    // Perform shift on this->input, set to this->output
+/**
+ * Perform shift left two operation. 
+ * Default output size is 32 bits, unless set in construction. 
+ */
+void ShiftLeftTwo::shiftLeftTwo()
+{
+    long inputLong = stol(this->input, nullptr, 2);
+    inputLong = inputLong << 2;
+
+    string bitString = "";
+
+    if(this->unitNum == 0) {
+        bitString = bitset<28>(inputLong).to_string();
+    } else if(this->unitNum == 1) {
+        bitString = bitset<32>(inputLong).to_string();
+    }
+    
+    this->output = bitString;
 }
--- ShiftLeftTwo.h
+++ ShiftLeftTwo.h
@@ -1,20 +1,20 @@
-#include<string>
+#include <string>
+#include <bitset>
 
 using namespace std;
 
-class ShiftLeftTwo {
+class ShiftLeftTwo
+{
 
-    public:
+public:
+    ShiftLeftTwo();            // default constructor
+    ShiftLeftTwo(int unitNum); // constructor to which ShiftLeftTwo unit
+    string get();              // get the shifted left input binary string as binary string
+    void set(string input);    // set the input binary string
 
-        // Constructor
-        ShiftLeftTwo();
-
-        // Perform shift left on input
-        string get();
-        void set(string input);
-
-    private:
-        string output;
-        string input;
-        void shiftLeftTwo();
+private:
+    string output;       // store output
+    string input;        // store input
+    int unitNum;         // store output bit string size
+    void shiftLeftTwo(); // perform shift function
 };
