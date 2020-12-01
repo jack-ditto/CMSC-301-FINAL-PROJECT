27434575203034e2ccebfbaf89b834df61e63771
okaynik <nikita.morozov@richmond.edu>
2020-11-30 22:42:37 -0500

    Fixed redirecting

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+
+class stream_redirection
+{
+    std::ostream& from;
+    std::ofstream to;
+    std::streambuf *const saved;
+public:
+    stream_redirection(std::ostream& from, const std::string& filename)
+        : from{from},
+          to{filename},
+          saved{from.rdbuf(to.rdbuf())}
+    {}
+    stream_redirection(const stream_redirection&) = delete;
+    void operator=(const stream_redirection&) = delete;
+    ~stream_redirection()
+    {
+        from.rdbuf(saved);
+    }
+};
+
+
 int main(int argc, char const *argv[])
 {
 	ConfigParser *parser;
@@ -18,19 +39,20 @@ int main(int argc, char const *argv[])
 	processor.setParameters(parser->debugMode(), parser->printMemoryContents(), parser->writeToFile(), parser->getFileName());
 
 	std::ofstream out("out.txt");
-    //std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+    std::streambuf *coutbuf = std::cout.rdbuf();
     std::cout.rdbuf(out.rdbuf());
 
 	try{
 		while (!processor.finishedExecution()){
 			processor.step();
-			if(parser->outputMode() == "sinle_step")
+			if(parser->outputMode() == "single_step")
 				cin.get(wait);
 		}
 	}catch(const std::invalid_argument& e){
 		std::cout << e.what() << std::endl;
 	}
 
+	std::cout.rdbuf(coutbuf); //reset to standard output again
 	out.close();
 
 	delete parser;
